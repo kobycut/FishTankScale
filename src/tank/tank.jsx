2e261dd12aa0341210fc0b058631cee44a54c519
kobycut <kobycut@gmail.com>
2024-11-08 16:36:24 -0700

    updates

--- src/tank/tank.jsx
+++ src/tank/tank.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export function Tank() {
   return (
     <main classNameName='container-fluid bg-secondary text-center'>
