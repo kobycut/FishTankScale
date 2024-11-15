2a94e9440d531d23032cbff7432daa22b9df0238
kobycut <kobycut@gmail.com>
2024-11-14 20:31:51 -0700

    login almost implemented?

--- service/index.js
+++ service/index.js
@@ -1,2 +1,55 @@
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+
+const port = process.argv.length > 2 ? process.argv[2] : 8080;
+
+let users = {};
+
 app.use(express.static('public'));
+app.use(express.json());
+
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+        users[user.email] = user;
+
+        res.send({ token: user.token });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+        if (req.body.password === user.password) {
+            user.token = uuid.v4();
+            res.send({ token: user.token });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+        delete user.token;
+    }
+    res.status(204).end();
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
