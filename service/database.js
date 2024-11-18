61db99b53003d4fa524eeb5240036a29ec37ad01
kobycut <kobycut@gmail.com>
2024-11-18 15:28:31 -0700

    dsafd

--- service/database.js
+++ service/database.js
@@ -2,12 +2,11 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-async function main() {
   // Connect to the database cluster
   const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
   const client = new MongoClient(url);
-  const db = client.db('rental');
-  const collection = db.collection('house');
+  const db = client.db('startup');
+  const userCollection = db.collection('user');
 
   // Test that you can connect to the database
   (async function testConnection() {
@@ -18,25 +17,29 @@ async function main() {
     process.exit(1);
   });
 
-  // Insert a document
-  const house = {
-    name: 'Beachfront views',
-    summary: 'From your bedroom to the beach, no shoes required',
-    property_type: 'Condo',
-    beds: 1,
+  function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+  }
+
+  module.exports = {
+    getUser,
+    getUserByToken,
+    createUser
   };
-  await collection.insertOne(house);
-
-  // Query the documents
-  const query = { property_type: 'Condo', beds: { $lt: 2 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-
-  const cursor = collection.find(query, options);
-  const rentals = await cursor.toArray();
-  rentals.forEach((i) => console.log(i));
-}
-
-main().catch(console.error);
--- service/index.js
+++ service/index.js
@@ -1,54 +1,84 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
-const uuid = require('uuid');
 const app = express();
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const DB = require('./database.js');
 
-let users = {};
+const authCookieName = 'token';
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
+app.set('trust proxy', true);
 
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 apiRouter.post('/auth/create', async (req, res) => {
-    const user = users[req.body.email];
-    if (user) {
+    if (await DB.getUser(req.body.email)) {
         res.status(409).send({ msg: 'Existing user' });
     } else {
-        const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-        users[user.email] = user;
+        const user = await DB.createUser(req.body.email, req.body.password);
 
-        res.send({ token: user.token });
+        // Set the cookie
+        setAuthCookie(res, user.token);
+
+        res.send({
+            id: user._id,
+        });
     }
 });
 
+
 apiRouter.post('/auth/login', async (req, res) => {
-    console.log("got to the endpoint");
-    const user = users[req.body.email];
+    const user = await DB.getUser(req.body.email);
     if (user) {
-        if (req.body.password === user.password) {
-            user.token = uuid.v4();
-            res.send({ token: user.token });
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({ id: user._id });
             return;
         }
     }
     res.status(401).send({ msg: 'Unauthorized' });
 });
 
-apiRouter.delete('/auth/logout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-        delete user.token;
-    }
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
     res.status(204).end();
+});
+
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+    const authToken = req.cookies[authCookieName];
+    const user = await DB.getUserByToken(authToken);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+});
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
 });
 
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
 
-app.listen(port, () => {
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+const httpService = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
