de7c8c0fc2f0c0cc2162bf57cfb168bc6d9a4f7b
Koby Cutler <kobycut@byu.edu>
2025-09-19 09:13:33 -0600

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/FishTankScale/',
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
